6d0a30b9e777f640a4b07ac805eced8a55cb8c63
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2015-07-18 17:02:45 +0900

    initial commit

---
+++ parse.go
@@ -0,0 +1,85 @@
+// Copyright 2015 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package datemaki
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Parse accepts contextful date format and returns absolute time.Time value.
+func Parse(value string) (time.Time, error) {
+	switch {
+	case strings.HasSuffix(value, "ago"):
+		return parseAgo(value)
+	}
+	return time.Now(), nil // TODO(ymotongpoo): replace actual time.
+}
+
+func splitTokens(value string) []string {
+	f := func(c rune) bool {
+		return c == rune(' ') || c == rune(',') || c == rune('.')
+	}
+	return strings.FieldsFunc(value, f)
+}
+
+// parseAgo parse "xxxx ago" format and returns corresponding absolute datetime.
+func parseAgo(value string) (time.Time, error) {
+	tokens := splitTokens(value)
+	now := time.Now()
+	for i := 0; i < len(tokens); i++ {
+		t := tokens[i]
+		if t == "ago" {
+			return now, nil
+		}
+		if i%2 == 0 {
+			n, err := strconv.Atoi(t)
+			if err != nil {
+				return now, fmt.Errorf("Format error: %v", t)
+			}
+			now, err := subDate(now, n, tokens[i+1])
+			if err != nil {
+				return now, err
+			}
+			i++
+		}
+	}
+	return now, nil
+}
+
+func subDate(t time.Time, n int, unit string) (time.Time, error) {
+	if strings.HasSuffix(unit, "s") {
+		unit = string([]byte(unit)[:len(unit)-1])
+	}
+	switch unit {
+	case "year":
+		return t.AddDate(-1*n, 0, 0), nil
+	case "month":
+		return t.AddDate(0, -1*n, 0), nil
+	case "week":
+		return t.AddDate(0, 0, -7*n), nil
+	case "day":
+		return t.AddDate(0, 0, -1*n), nil
+	case "hour":
+		return t.Add(time.Duration(-1*n) * time.Hour), nil
+	case "minute":
+		return t.Add(time.Duration(-1*n) * time.Minute), nil
+	case "second":
+		return t.Add(time.Duration(-1*n) * time.Second), nil
+	default:
+		return t, fmt.Errorf("Unsupported time unit: %v", unit)
+	}
+}
---
+++ parse_test.go
@@ -0,0 +1,53 @@
+// Copyright 2015 Yoshi Yamaguchi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package datemaki
+
+import (
+	"strings"
+	"testing"
+)
+
+var relativeAgoTests = []string{
+	"2 seconds ago",
+	"3 minutes ago",
+	"4 hours ago",
+	"5 days ago",
+	"1 week ago",
+	"2 months ago",
+	"1 year, 3 months ago",
+	"1.year.4.months.ago",
+	"2.years.ago",
+}
+
+func TestSplitTokens(t *testing.T) {
+	for i, test := range relativeAgoTests {
+		pre1 := strings.Replace(test, ",", " ", -1)
+		pre2 := strings.Replace(pre1, ".", " ", -1)
+		words := strings.Fields(pre2)
+		tokens := splitTokens(test)
+		if len(words) != len(tokens) {
+			t.Errorf("#%d: word counts are different, %d is expected, got %d", i, len(words), len(tokens))
+		}
+	}
+}
+
+func TestParseAgo(t *testing.T) {
+	for i, test := range relativeAgoTests {
+		parsed, err := parseAgo(test)
+		if err != nil {
+			t.Errorf("#%v: %v", i, err)
+		}
+		t.Logf("#%v: parsed: %v (%v)", i, parsed, test)
+	}
+}
